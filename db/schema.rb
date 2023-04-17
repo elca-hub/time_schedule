e22845d86f95b137e831bf30da556e4d1b5ee816
elca-hub <csubaru07@gmail.com>
2023-04-18 00:04:58 +0900

    パスワードをbcyrptにて暗号化するためにテーブル構造を変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_08_085000) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_17_150107) do
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "pw"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
