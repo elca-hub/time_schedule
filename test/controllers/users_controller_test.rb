16a8e5b6a17c9b496534b9ba4fe5481756bf13e5
elca-hub <csubaru07@gmail.com>
2023-04-23 00:24:20 +0900

    テストケースの調整

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,16 @@
 require "test_helper"
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  # 同じ名前の人を追加することができないか検証
+  test "should do not insert same name user" do
+    user = User.new(name: "test", password: "user")
+
+    begin
+      User.create(name: user.name, password: user.password)
+
+      User.create(name: user.name, password: user.password)
+    rescue => exception
+      assert exception.class == ActiveRecord::RecordNotUnique
+    end
+  end
 end
