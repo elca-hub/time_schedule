4a071af836bd8f5ee548c9b21140b1826207cd66
elca-hub <csubaru07@gmail.com>
2023-04-25 12:46:55 +0900

    テストケースの追加

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -13,4 +13,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
       assert exception.class == ActiveRecord::RecordNotUnique
     end
   end
+
+  # ユーザモデルのみからタスクを取得することが可能か検証
+  test "should fetch task only use user model" do
+    user = User.new(name: "test", password: "user")
+
+    task = Task.new(title: "task test", content: "this is test task")
+
+    user.save
+    task.save
+    user_task = UserTask.new(user_id: user.id, task_id: task.id)
+    user_task.save
+
+    test_user = User.find(user.id)
+    assert test_user.tasks[0] == task
+  end
 end
