2d7e6913ae9baa20fab8737dc67fc33ab6fd4f53
elca-hub <csubaru07@gmail.com>
2023-04-27 10:36:45 +0900

    dbの構成変更

--- db/migrate/20230420235938_create_tasks.rb
+++ db/migrate/20230420235938_create_tasks.rb
@@ -3,6 +3,7 @@ class CreateTasks < ActiveRecord::Migration[7.0]
     create_table :tasks do |t|
       t.string :title, null: false
       t.string :content, null: false
+      t.boolean :is_done, null: false, default: false
 
       t.timestamps
     end
--- db/migrate/20230421000453_create_user_tasks.rb
+++ db/migrate/20230421000453_create_user_tasks.rb
@@ -3,6 +3,7 @@ class CreateUserTasks < ActiveRecord::Migration[7.0]
     create_table :user_tasks do |t|
       t.references :user, null: false, foreign_key: true
       t.references :task, null: false, foreign_key: true
+      t.boolean :is_owner, null: false, default: false
 
       t.timestamps
     end
