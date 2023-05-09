37ff9f58ff8a40390bb2d4958e2c00aa4a6a8e90
elca-hub <csubaru07@gmail.com>
2023-05-09 16:13:44 +0900

    Taskモデルに制約を追加

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,6 @@
 class Task < ApplicationRecord
     has_many :user_tasks
     has_many :users, through: :user_tasks
+    validates :title, uniqueness: true, presence: true
+    validates :content, uniqueness: true, presence: true
 end
