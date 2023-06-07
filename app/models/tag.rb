94cb7d75ea462eaaa248999ab3ec411ecaa9a775
elca-hub <csubaru07@gmail.com>
2023-06-07 16:53:25 +0900

    modelの修正

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,6 +1,6 @@
 class Tag < ApplicationRecord
     belongs_to :user
-    
+
     has_many :tasks, dependent: :destroy
 
     validates :name, presence: true, length: {maximum:100}, uniqueness: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :task, dependent: :destroy
-  has_many :tag, dependent: :destroy
+  has_many :tags, dependent: :destroy
+  has_many :tasks, dependent: :destroy
 
   validates :name, length: {maximum:100}, presence: true
   validates :email, presence: true, uniqueness: true
