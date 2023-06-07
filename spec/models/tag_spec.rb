a23552e052173da89c059705db96722e683aa254
elca-hub <csubaru07@gmail.com>
2023-06-07 13:48:20 +0900

    tagのテストケースを追加

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,5 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Tag, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:tag) { build(:tag) }
+  subject { tag }
+
+  describe "バリデーション関連" do
+    context "すべての属性が完璧なアイドルな場合" do
+      it "有効" do
+        is_expected.to be_valid
+      end
+    end
+
+    describe "名称" do
+      context "名称が空文字の場合" do
+        it "無効" do
+          tag.name = ""
+
+          is_expected.not_to be_valid
+        end
+      end
+
+      context "名称が100文字以下の場合" do
+        it "有効" do
+          tag.name = "a" * 100
+
+          is_expected.to be_valid
+        end
+      end
+
+      context "名称が101文字以上の場合" do
+        it "無効" do
+          tag.name = "a" * 101
+
+          is_expected.not_to be_valid
+        end
+      end
+
+      context "名称が重複している場合" do
+        before do
+          create(:tag, name: tag.name)
+        end
+
+        it "無効" do
+          is_expected.not_to be_valid
+        end
+      end
+    end
+  end
 end
