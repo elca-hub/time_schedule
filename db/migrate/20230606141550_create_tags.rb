bc9cb4bb88cefa59903ef011c8ce74bd53af1b97
elca-hub <csubaru07@gmail.com>
2023-06-08 17:17:49 +0900

    tagsにカラーコードと説明文を追加

--- db/migrate/20230606141550_create_tags.rb
+++ db/migrate/20230606141550_create_tags.rb
@@ -3,6 +3,8 @@ class CreateTags < ActiveRecord::Migration[7.0]
     create_table :tags do |t|
       t.string :user_id, null: false
       t.string :name, null: false
+      t.string :color_code, null: false
+      t.string :description, null: true, default: nil
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_06_141550) do
   create_table "tags", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "user_id", null: false
     t.string "name", null: false
+    t.string "color_code", null: false
+    t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
