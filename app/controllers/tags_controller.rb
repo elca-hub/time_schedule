55031488c83f8058397d0140d90ba0741326ac6e
elca-hub <csubaru07@gmail.com>
2023-06-08 22:27:32 +0900

    タグの内容を更新する処理を追加

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -22,6 +22,19 @@ class TagsController < ApplicationController
     end
   end
 
+  def edit
+    @tag = Tag.find(params[:id])
+  end
+
+  def update
+    @tag = Tag.find(params[:id])
+
+		@tag.update(tag_params)
+
+    redirect_to tasks_path
+  end
+  
+
   private
   	def tag_params
   		params.require(:tag).permit(:name, :color_code, :description)
