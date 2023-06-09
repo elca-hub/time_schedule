cd2843c273b1632172a6dd1d3ad7459a1e700344
elca-hub <csubaru07@gmail.com>
2023-06-09 11:22:56 +0900

    applicationcontrollerにincludeを追加し、get_view_errorをすべてのcontrollerで読み込むように修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller? 
-    before_action :get_view_error
+  include ErrorMessage
+  before_action :configure_permitted_parameters, if: :devise_controller? 
+  before_action :get_view_error
 
-    def after_sign_in_path_for(resource)
-        "/users/"
+  def after_sign_in_path_for(resource)
+    "/users/"
+  end
+
+  private
+  
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+      devise_parameter_sanitizer.permit(:account_update,keys:[:name])
     end
-
-    private
-    
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
-        devise_parameter_sanitizer.permit(:account_update,keys:[:name])
-      end
 end
