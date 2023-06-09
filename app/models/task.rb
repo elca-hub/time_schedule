4083770f57ba1eb2315010402c2b02d398b6c4a6
elca-hub <csubaru07@gmail.com>
2023-06-09 11:29:02 +0900

    contentを空の場合でも有効にし、最大文字列を500文字に変更

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,5 +3,5 @@ class Task < ApplicationRecord
     belongs_to :tag
 
     validates :title, length: {maximum:100}, presence: true
-    validates :content, length: {maximum:255}, presence: true
+    validates :content, length: {maximum:500}
 end
