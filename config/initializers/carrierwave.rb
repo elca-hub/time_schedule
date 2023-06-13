fdf7b761bc04f036491441c2e33006fa2b9bbd35
elca-hub <csubaru07@gmail.com>
2023-06-13 15:20:54 +0900

    carrierwaveのinitializerの内容を修正

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,21 +5,20 @@ require 'carrierwave/storage/fog'
 # 保存先の分岐
 CarrierWave.configure do |config|
   # 本番環境はS3に保存
-  if Rails.env.production? || Rails.env.development?
+  if Rails.env.production?
     config.storage = :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'elca-test-bucket'
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/elca-test-bucket'
+    config.fog_directory  = Rails.application.credentials.aws[:bucket]
+    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.aws[:bucket]}'
     # iam_profile
     config.fog_credentials = {
       provider: 'AWS',
-      # credentialsで管理
       aws_access_key_id: Rails.application.credentials.aws[:access_key_id],
       aws_secret_access_key: Rails.application.credentials.aws[:secret_access_key],
-      region: 'ap-northeast-1' #東京リージョン
+      region: 'ap-northeast-1'
     }
-    # キャッシュをS3に保存
-    # config.cache_storage = :fog
+
+    config.cache_storage = :fog
   else
     # 開発環境はlocalに保存
     config.storage :file
