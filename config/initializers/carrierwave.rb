e1b5225a3926f31f3a40ecbd56243aade0bd2c40
elca-hub <csubaru07@gmail.com>
2023-06-13 16:46:59 +0900

    シングルからダブルクォートへ

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -9,7 +9,7 @@ CarrierWave.configure do |config|
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_directory  = Rails.application.credentials.aws[:bucket]
-    config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.aws[:bucket]}'
+    config.asset_host = "https://s3-ap-northeast-1.amazonaws.com/#{Rails.application.credentials.aws[:bucket]}"
     # iam_profile
     config.fog_credentials = {
       provider: 'AWS',
