ecfb92e0d10e62d9570a4d49d9f186213a3ce35a
elca-hub <csubaru07@gmail.com>
2023-06-21 15:59:53 +0900

    ユーザが新規登録した際に、登録完了のメールを配信するように設定

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -6,4 +6,11 @@ class UserMailer < ApplicationMailer
         @url  = 'https://playground.elca-web.com/'
         mail(to: @user.email, subject: 'これはテスト用のメール内容となります。')
     end
+
+    def welcome_mail
+        @user = params[:user]
+        @root_url = params[:root_url]
+
+        mail(to: @user.email, subject: "【ご登録完了のお知らせ】ようこそ、timeScheduleへ!")
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "homes#index"
-  devise_for :users, controllers: {   registrations: 'users/registrations' }
+  devise_for :users, controllers: {
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations'
+  }
 
   get "/users/" => "users#index"
 
