e98f3ad68a041d1955fabdad723b003a0d292dd5
elca-hub <csubaru07@gmail.com>
2023-06-21 15:59:12 +0900

    テスト用に作成していたメソッドの削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,4 @@ class UsersController < ApplicationController
 	def index
 		@user = current_user
 	end
-
-	def send_mail
-		UserMailer.with(user: current_user).test_mail.deliver_later
-
-		redirect_to users_path, notice: "テスト用のメール配信が完了しました。"
-	end
 end
