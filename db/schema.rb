c683deab51512ea3f2f8f90f0c8c9c0b4760257d
elca-hub <csubaru07@gmail.com>
2023-07-12 15:49:07 +0900

    スキーマのアップデート

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_12_073427) do
+ActiveRecord::Schema[7.0].define(version: 2023_07_12_061902) do
   create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,7 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_12_073427) do
     t.string "user_id", null: false
     t.string "name", null: false
     t.string "color_code", null: false
-    t.string "description"
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
