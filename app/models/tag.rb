a19a5b3e9d1e1654d191ea418d6aca6383eac89a
elca-hub <csubaru07@gmail.com>
2023-07-12 15:48:56 +0900

    DBのtextにはデフォルト値を設定することができないので、モデル側でデフォルト値を空文字に設定

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,6 +3,11 @@ class Tag < ApplicationRecord
 
     has_many :tasks, dependent: :destroy
 
+    # descriptionが指定されていない場合は空文字を設定する
+    before_validation do
+        self.description = "" if self.description.nil?
+    end
+
     validates :name, presence: true, length: {maximum:100}, uniqueness: true
     validates :color_code, presence: true, length: {maximum:7}
     validates :description, length: {maximum:500}
